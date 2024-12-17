ce20d0c6116e105fa122048c831b22266cfc0bfe
Eike <eike.rackwitz@mail.de>
2024-12-17 22:23:07 +0100

    added org

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @organisation = Organisation.first
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_17_211421) do
+  create_table "organisations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
